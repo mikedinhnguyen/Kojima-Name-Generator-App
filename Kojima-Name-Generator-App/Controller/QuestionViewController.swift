7088391bbd32b30063012f186cbb75775a89a4d6
Mike <mnguyen287@ucmerced.edu>
2020-08-25 11:37:46 -0700

    progress bar + char limit

--- Kojima-Name-Generator-App/Controller/QuestionViewController.swift
+++ Kojima-Name-Generator-App/Controller/QuestionViewController.swift
@@ -14,6 +14,8 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerField: UITextField!
     @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var snakeBox: UIImageView!
     
     var questionManager = QuestionManager()
     var nameCalculator = NameCalculator()
@@ -39,6 +41,8 @@ class QuestionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+        progressBar.progress = 0.0
         
         answerField.delegate = self
         pickerView.delegate = self
@@ -50,6 +54,7 @@ class QuestionViewController: UIViewController {
     func nextQuestion() {
         if questionManager.index < questionManager.questionairre.count - 1 {
             questionManager.index += 1
+            
             if (!questionManager.isPicker()) { // text field
                 // change to text field
                 answerField.isHidden = false
@@ -59,6 +64,11 @@ class QuestionViewController: UIViewController {
                 // reset answer field
                 answerField.text = ""
                 answerField.placeholder = "Answer goes here"
+                
+                // number pad
+//                if questionManager.index == 5 {
+//                    answerField.keyboardType = .asciiCapableNumberPad
+//                }
             } else if (questionManager.isPicker()) { // picker
                 // change to picker
                 pickerView.isHidden = false
@@ -77,48 +87,99 @@ class QuestionViewController: UIViewController {
         }
     }
     
-    // mp3 alert
-    
-    func playSound(soundName: String) {
-        let url = Bundle.main.url(forResource: soundName, withExtension: "mp3")
-        player = try! AVAudioPlayer(contentsOf: url!)
-        player!.play()
-    }
-    
     @IBAction func continuePressed(_ sender: UIButton) {
         if answerField.isHidden {
             if pickerView.isHidden {
                 return
             }
             
-            // check if 0
+            // check if picker is not "Select One" option
             if (pickerView.selectedRow(inComponent: 0) == 0) {
-                print("not possible")
+                presentAlert(isPicker: true)
                 playSound(soundName: K.MGS)
                 return
             }
             
             input = pickerValues[pickerCount][pickerView.selectedRow(inComponent: 0)]
             pickerCount += 1
+            
+            // store answer somewhere and move on
             nameCalculator.storeAnswers(input)
             nextQuestion()
+            progressSnake()
             return
         }
         if answerField.text! != "" {
-            // store answer somewhere!
+            // store answer somewhere and move on
             nameCalculator.storeAnswers(answerField.text!)
-            
-            // next question
             nextQuestion()
+            progressSnake()
             
         } else {
+            presentAlert(isPicker: false)
             playSound(soundName: K.MGS)
             answerField.placeholder = "Please type something!"
         }
-        
     }
 
+    // MARK: - Sound + Alert Methods
+
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player!.play()
+    }
+
+    func presentAlert(isPicker: Bool) {
+        if (isPicker){
+            let alert = UIAlertController(title: "You have not picked anything yet.", message: "Please pick an option in order to continue.", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: nil))
+
+            present(alert, animated: true, completion: nil)
+        } else {
+            
+            let alert = UIAlertController(title: "You have not entered anything yet.", message: "Please type something in order to continue.", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: nil))
+
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - Progress Bar
+    func progressSnake() {
+        let xPosition = snakeBox.frame.origin.x + 15 // Slide right + 15px
+        let yPosition = snakeBox.frame.origin.y
+
+        let width = snakeBox.frame.size.width
+        let height = snakeBox.frame.size.height
+        
+        UIView.animate(withDuration: 1.0) {
+            self.progressBar.setProgress(Float(self.questionManager.index) / Float(self.questionManager.questionairre.count), animated: true)
+        }
+
+        UIView.animate(withDuration: 1.0, animations: {
+            self.snakeBox.frame = CGRect(x: xPosition, y: yPosition, width: width, height: height)
+        })
+    }
 }
+
+// MARK: - Dismiss Keyboard
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+
 
 // MARK: - UITextField Methods
 
@@ -135,6 +196,15 @@ extension QuestionViewController: UITextFieldDelegate {
             answerField.placeholder = "Please type something!"
             return false
         }
+    }
+    // set character limit to 40
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentCharacterCount = textField.text?.count ?? 0
+        if range.length + range.location > currentCharacterCount {
+            return false
+        }
+        let newLength = currentCharacterCount + string.count - range.length
+        return newLength <= 40
     }
 }
 
